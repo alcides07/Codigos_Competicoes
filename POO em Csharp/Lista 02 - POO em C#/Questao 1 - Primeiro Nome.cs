577c187a6e11d01a8db027ee18ecd584f397e1c5
Alcides Dantas <alcidesdantasdj@gmail.com>
2022-07-15 23:17:57 -0300

    Delete POO em Csharp directory

--- POO em Csharp/Lista 02 - POO em C#/Questao 1 - Primeiro Nome.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Digite seu primeiro nome:");
-    string nome = Console.ReadLine();
-    Console.WriteLine($"Bem-vindo ao C#, {nome}");
-  }
-}
--- POO em Csharp/Lista 02 - POO em C#/Questao 2 - Nome Completo.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Digite seu nome completo:");
-    string[] nome = Console.ReadLine().Split(' ');
-    Console.WriteLine($"Bem-vindo ao c#, {nome[0]}");
-  }
-}
